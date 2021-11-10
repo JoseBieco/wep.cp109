ee90cd28e5f4d74c1c456fff06114518013689f8
Jose Bieco <jo_bieco@hotmail.com>
2021-11-10 09:32:10 -0300

    build: vuetify

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,13 +11,14 @@ const routes: Array<RouteConfig> = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/schedule",
+    name: "Schedule",
+    component: Home,
+  },
+  {
+    path: "/contacts",
+    name: "Contacts",
+    component: Home,
   },
 ];
 
