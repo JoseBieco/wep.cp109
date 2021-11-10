76e3eab1eaed7de659c294ef508b0588a58ef15b
Jose Bieco <jo_bieco@hotmail.com>
2021-11-10 09:33:06 -0300

    feat(components): header

---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="page">
+    <v-system-bar color="#536DFE"></v-system-bar>
+
+    <v-app-bar color="#304FFE" elevation="4">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="poninterText" @click="goTo('/')"
+        >Menu</v-toolbar-title
+      >
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Menu </v-list-item-title>
+          <v-list-item-subtitle> List items </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item
+            v-for="(item, index) in routes"
+            :key="index"
+            @click="goTo(item.path)"
+          >
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Header",
+
+  data: () => ({
+    drawer: false,
+    group: null,
+    routes: [
+      { name: "Home", path: "/" },
+      { name: "Agenda", path: "schedule" },
+      { name: "Contatos", path: "contacts" },
+    ],
+  }),
+
+  methods: {
+    goTo(path: string): void {
+      this.$router.push(path);
+    },
+  },
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+});
+</script>
+
+<style scoped>
+.page {
+  overflow: hidden;
+}
+
+.poninterText:hover {
+  cursor: pointer;
+}
+</style>
