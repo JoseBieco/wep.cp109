01a6e01c6cddfe7638a9d233fd149f0b42dc69c9
JoseBieco <jo_bieco@hotmail.com>
2021-11-26 18:39:58 -0300

    feat(src/store): create naruto state and get data from api

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,31 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    naruto: [],
+  },
+  mutations: {
+    SET_NARUTO(state, data) {
+      state.naruto = data;
+    },
+  },
+  actions: {
+    getNaruto({ commit }) {
+      axios
+        .get(
+          "https://animechan.vercel.app/api/quotes/anime?title=naruto&page=1",
+        )
+        .then((response) => {
+          commit("SET_NARUTO", response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
   modules: {},
 });
