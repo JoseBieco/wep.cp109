625c72abf9d0c21ca40e104d107785e58b026b48
JoseBieco <jo_bieco@hotmail.com>
2021-11-26 21:44:13 -0300

    feat(src/store): load animes

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,26 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import axios from "axios";
+import { IAnimes } from "../interfaces/animes.interface";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     naruto: [],
+    animes: [] as any,
   },
   mutations: {
     SET_NARUTO(state, data) {
       state.naruto = data;
+    },
+
+    ADD_ANIME(state, data) {
+      state.animes.push(data);
+    },
+
+    CLEAR_ANIMES(state) {
+      state.animes = [];
     },
   },
   actions: {
@@ -24,6 +34,31 @@ export default new Vuex.Store({
         })
         .catch((error) => {
           console.log(error);
+        });
+    },
+
+    loadAnimes({ commit, dispatch }) {
+      commit("CLEAR_ANIMES");
+      axios
+        .get("https://anime-facts-rest-api.herokuapp.com/api/v1")
+        .then((response) => {
+          response.data.data.forEach((anime: any) => {
+            const item: IAnimes = {} as IAnimes;
+            item.animeDescription = { ...anime };
+            dispatch("loadFactsFromAnime", item);
+          });
+        });
+    },
+
+    loadFactsFromAnime({ commit }, anime: IAnimes) {
+      axios
+        .get(
+          "https://anime-facts-rest-api.herokuapp.com/api/v1/" +
+            anime.animeDescription.anime_name,
+        )
+        .then((response) => {
+          anime.facts = response.data.data.map((item: any) => item.fact);
+          commit("ADD_ANIME", anime);
         });
     },
   },
