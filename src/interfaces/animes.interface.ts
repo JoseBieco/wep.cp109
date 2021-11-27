0c9a2c420d0c06b7f98f48ab871eb351372bed1e
JoseBieco <jo_bieco@hotmail.com>
2021-11-26 23:25:46 -0300

    feat(src/interfaces): IOnePieceCharacter

--- src/interfaces/animes.interface.ts
+++ src/interfaces/animes.interface.ts
@@ -8,3 +8,17 @@ interface IAnimeDescruption {
   anime_name: string;
   anime_img: string;
 }
+
+export interface IOnePieceCharacter {
+  character: IPersonalInfo;
+  position: string;
+  akuma_no_mi: string;
+  bounty: number;
+}
+
+interface IPersonalInfo {
+  name: string;
+  img: string;
+  blood_type: string;
+  age: number;
+}
