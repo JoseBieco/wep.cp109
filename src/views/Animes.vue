b778550d4c7496c191dc9f384463f36e28ca2e83
JoseBieco <jo_bieco@hotmail.com>
2021-11-26 23:51:32 -0300

    feat(src/views): new pages

---
+++ src/views/Animes.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <AnimeCard
+      v-for="anime in animes"
+      :key="anime.animeDescription.anime_id"
+      :img_url="anime.animeDescription.anime_img"
+      :facts="anime.facts"
+      :character="anime.animeDescription.anime_name"
+      anime=""
+    >
+    </AnimeCard>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import AnimeCard from "../components/AnimeCard.vue";
+
+export default Vue.extend({
+  name: "Home",
+  components: {
+    AnimeCard,
+  },
+  data: function () {
+    return {};
+  },
+
+  methods: {},
+
+  computed: {
+    animes() {
+      return this.$store.state.animes;
+    },
+  },
+
+  mounted() {
+    this.$store.dispatch("loadAnimes");
+  },
+});
+</script>
+
+<style scoped>
+.container {
+  height: 100%;
+  width: 80%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-start;
+}
+</style>
---
+++ src/views/OnePiece.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <OnePieceCard
+      v-for="(character, index) in characters"
+      :key="index"
+      :character="character"
+    ></OnePieceCard>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import OnePieceCard from "../components/OnePieceCard.vue";
+
+export default Vue.extend({
+  name: "OnePiece",
+  components: { OnePieceCard },
+  computed: {
+    characters() {
+      return this.$store.state.one_piece;
+    },
+  },
+});
+</script>
+
+<style scoped>
+.container {
+  height: 100%;
+  width: 80%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-start;
+}
+</style>
